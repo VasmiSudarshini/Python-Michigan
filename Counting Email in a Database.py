a698a035fa689347d7c6e7f726856919a0b7bf95
Vasmi <86046947+VasmiSudarshini@users.noreply.github.com>
2023-04-05 16:21:37 +0530

    Add files via upload
    This application will read the mailbox data (mbox.txt) and count the number of email messages per organization (i.e. domain name of the email address) using a database with the following schema to maintain the counts.
    
    CREATE TABLE Counts (org TEXT, count INTEGER)
    When you have run the program on mbox.txt upload the resulting database file above for grading.
    If you run the program multiple times in testing or with different files, make sure to empty out the data before each run.
    
    You can use this code as a starting point for your application: http://www.py4e.com/code3/emaildb.py.
    
    The data file for this application is the same as in previous assignments: http://www.py4e.com/code3/mbox.txt.
    
    Because the sample code is using an UPDATE statement and committing the results to the database as each record is read in the loop, it might take as long as a few minutes to process all the data. The commit insists on completely writing all the data to disk every time it is called.
    
    The program can be speeded up greatly by moving the commit operation outside of the loop. In any database program, there is a balance between the number of operations you execute between commits and the importance of not losing the results of operations that have not yet been committed.

---
+++ Counting Email in a Database.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+#If the database (emaildb.sqlite) does not exist, then it’ll be created. 
+conn = sqlite3.connect('emaildb.sqlite') # creating and connecting to the database
+cur = conn.cursor() # similar to file handle
+
+cur.execute('DROP TABLE IF EXISTS Counts') # Droping the table (if it already exists) that we are about to create
+
+cur.execute('CREATE TABLE Counts (org TEXT, count INTEGER)')
+
+fname = input('Enter-')
+fh = open(fname)
+
+if len(fname) <0:
+    fh = open('mbox.txt')
+
+for i in fh:
+    if i.startswith('From:'):
+        pieces = i.split()
+        email_id = pieces[1]
+        domain_name = email_id.split('@')[1]
+        cur.execute('SELECT count FROM Counts WHERE org = ?', (domain_name,)) # going to read this like a file or bunch of data # this line doesnt do anything much
+        row = cur.fetchone() #row is none when u print
+        if row is None:
+            cur.execute('INSERT INTO Counts (org,count) VALUES (?, 1)', (domain_name,))
+        else:
+            cur.execute('UPDATE Counts SET count = count+1 WHERE org = ?', (domain_name,))
+
+#additions = 0   
+#for i in email_id:
+ #   if i in email_id:
+  #      print(email_id)
+        #cur.execute('INSERT INTO Counts (org, count) VALUES (email, additions+1 ), (email,)')
+    #else:
+        #cur.execute('INSERT INTO Counts (org, count) VALUES (email, 1 ),(email,)')
+#print(cur)
+
+conn.commit()
+
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), str(row[1]))
+
+cur.close()
